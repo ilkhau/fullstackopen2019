d95925ab3c685e4f51029b7869567974ee36de12
Ilkka Hauta-aho <ilkka.hauta-aho@miiliconsulting.com>
2019-04-13 20:10:41 +0300

    Step 9 and separate module

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,105 +1,62 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Header, Course} from './components/Course'
 
-const Header = ({course}) => {
+const App = () => {
 
-    return (
-        <h1>
-            {course.name}
-        </h1>
-    )
-};
+    const courses = [
+        {
+            name: 'Half Stack -sovelluskehitys',
+            id: 1,
+            parts: [
+                {
+                    name: 'Reactin perusteet',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Tiedonvälitys propseilla',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'Komponenttien tila',
+                    exercises: 14,
+                    id: 3
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewaret',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ];
 
-const Part = ({part}) => {
-    console.log(part);
-    return (
-        <div>
-            <p>
-                {part.name} {part.exercises}
-            </p>
-        </div>
-    )
-};
+    console.log(courses);
 
-const Total = ({course}) => {
-
-    console.log("total");
-    const exercises = () => course.parts.reduce((prev, cur ) => {
-        return prev + cur.exercises;
-    }, 0);
-
-    console.log("E ", exercises());
-
-    return (
-        <div>
-            <p>yhteensä {exercises()} tehtävää</p>
-        </div>
-    )
-};
-
-const Course = ({course}) => {
-
-    return (
-        <div>
-            <Header course={course}/>
-            <Content course={course} />
-        </div>
-    )
-
-};
-
-const Content = ({course}) => {
-
-    console.log(course);
-    console.log(course.parts);
-
-    const parts = () => course.parts.map(part => {
-        console.log(part);
-        return (<Part key={part.id} part={part}/>)
+    const printCourses = () => courses.map(course => {
+        console.log(course);
+        return (<Course key={course.id} course={course} />)
     });
 
     return (
         <div>
-            {parts()}
-            <Total course={course} />
+            <Header text="Opetusohjelma" />
+            {printCourses()}
         </div>
     )
 };
 
-const App = ({course}) => {
-    console.log(course);
-    return (
-        <div>
-            <Course course={course}/>
-        </div>
-    )
-};
-
-const course = {
-    name: 'Half Stack -sovelluskehitys',
-    parts: [
-        {
-            name: 'Reactin perusteet',
-            exercises: 10,
-            id: 1
-        },
-        {
-            name: 'Tiedonvälitys propseilla',
-            exercises: 7,
-            id: 2
-        },
-        {
-            name: 'Komponenttien tila',
-            exercises: 14,
-            id: 3
-        },
-        {
-            name: 'Uusi course',
-            exercises: 10,
-            id: 4
-        }
-    ]
-};
-
-console.log(course);
-ReactDOM.render(<App course={course}/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
